9d11d2bb3118a30b025b212a5e24013590fb6752
Barbyie <thiago.barbieri2002@gmail.com>
2024-01-13 15:17:27 -0300

    Update keybinds for splitting vertically and horizontaly

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,3 +12,9 @@ keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate Down
 keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
 keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 
+-- Window Management
+
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally 
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
+
