04fd0270569a257fbaca9bbd7ca92f4e6ffd80f9
Barbyie <thiago.barbieri2002@gmail.com>
2024-01-12 23:14:22 -0300

    Add keybinds

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+local keymap = vim.keymap
+
+-- Directory Navigation
+
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- Pane Navigation
+
+keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate left
+keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate Down
+keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
+keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
+
